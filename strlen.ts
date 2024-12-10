31e3cf6a8ec1cb85e5b17028b114fba6d08d6305
Balajiraje <131636786+Balajiraje@users.noreply.github.com>
2024-12-09 19:37:52 -0800

    Add files via upload

---
+++ strlen.ts
@@ -0,0 +1,3 @@
+let uname = new String("Hello TS");  
+console.log("Message: " + uname);  
+console.log("Length: " + uname.length);
---
+++ varassign.ts
@@ -0,0 +1,9 @@
+type Pet = 'mouse' | 'dog' | 'Rabbit';  
+let pet: Pet;  
+if(pet = 'mouse'){  //assigned Inn
+    console.log("Correct");  
+};  
+if(pet = 'Deer')  //type pet la assign aagala
+{  
+    console.log(" Out of type");  
+}; 
